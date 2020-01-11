3bf95a822f81c99e0c83f20d6fe8a7cd5bb58250
Sazzad <sajjad.cse.diu@gmail.com>
2020-01-11 23:58:02 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <router-link to="/helloworld">Home</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+
+<script>
+import Router from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+
+const routes = [
+  { path: '/helloworld', component: HelloWorld }
+]
+
+const router = new Router({
+routes: routes,
+mode: 'history'
+})
+
+export default {
+  name: 'app',
+  router
+}
+</script>
+
+<style>
+
+</style>
